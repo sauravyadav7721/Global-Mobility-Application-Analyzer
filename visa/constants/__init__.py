30dbc3e0265d4d47f9f287c82d883a63a432b4dc
sauravyadav7721 <sauravyadav7721@gmail.com>
2026-05-07 02:28:10 +0530

    app.py file added

--- visa/constants/__init__.py
+++ visa/constants/__init__.py
@@ -24,7 +24,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "us-east-1"
+REGION_NAME = "eu-north-1"
 
 
 DATA_INGESTION_COLLECTION_NAME:str = "visa_data"
@@ -44,3 +44,18 @@ DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
 
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "myvisabucket26"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST ="0.0.0.0"
+APP_PORT = 8080
